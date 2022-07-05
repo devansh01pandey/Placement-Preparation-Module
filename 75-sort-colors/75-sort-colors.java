87a7700f672e2cc184013109f7bb97d8840b3710
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-07-05 22:11:37 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (29.00%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -32,37 +32,7 @@ class Solution {
         
         
         
-// DUTCH NATIONAL FLAG ALGO.                
-//         var low = 0;
-//         var mid = 0;
-//         var high = nums.length-1;
-        
-//         while(mid<=high)
-//         {
-//             if(nums[mid] == 0)
-//             {
-//                 swap(nums,mid,low);
-//                 low++;
-//                 mid++;
-//             }
-//             else if (nums[mid] == 1)
-//             {
-//                 mid++;
-//             }
-//             else
-//             {
-//                 swap(nums,mid,high);
-//                 high--;
-//             }
-//         }
-        
-//     }
-    
-//     private static void swap(int arr[],int first,int second){
-//         int temp = arr[first];
-//         arr[first] = arr[second];
-//         arr[second] = temp;
-//     }
+
 
 }
 }
