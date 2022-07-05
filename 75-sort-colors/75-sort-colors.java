133fa79f9d3f8ef162aed1116a22eafbb360ee8b
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-07-05 22:09:04 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (21.07%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,42 +1,84 @@
 class Solution {
     public void sortColors(int[] nums) {
-        
         var low = 0;
-        var mid = 0;
+        var mid=0;
         var high = nums.length-1;
+        int temp;
         
-        while(mid<=high)
-        {
-            if(nums[mid] == 0)
-            {
-                swap(nums,mid,low);
-                low++;
-                mid++;
+        while(mid <= high) {
+            switch(nums[mid]) {
+                case 0 :{
+                    temp = nums[low];
+                    nums[low] = nums[mid];
+                    nums[mid] = temp;
+                    low++;
+                    mid++;
+                    break;
+                }
+                case 1: {
+                    mid++;
+                    break;
+                }
+                case 2: {
+                    temp = nums[mid];
+                    nums[mid] = nums[high];
+                    nums[high] = temp;
+                    high--;
+                    break;
+                }
             }
-            else if (nums[mid] == 1)
-            {
-                mid++;
-            }
-            else
-            {
-                swap(nums,mid,high);
-                high--;
-            }
+                
         }
         
-    }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         var low = 0;
+//         var mid = 0;
+//         var high = nums.length-1;
+        
+//         while(mid<=high)
+//         {
+//             if(nums[mid] == 0)
+//             {
+//                 swap(nums,mid,low);
+//                 low++;
+//                 mid++;
+//             }
+//             else if (nums[mid] == 1)
+//             {
+//                 mid++;
+//             }
+//             else
+//             {
+//                 swap(nums,mid,high);
+//                 high--;
+//             }
+//         }
+        
+//     }
     
-    private static void swap(int arr[],int first,int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] = temp;
-    }
+//     private static void swap(int arr[],int first,int second){
+//         int temp = arr[first];
+//         arr[first] = arr[second];
+//         arr[second] = temp;
+//     }
     
-    private static void print(int nums[]) {
-        for(int num : nums) 
-        {
-            System.out.print(nums+" ");
-        }
-        System.out.println();
-    }
+//     // private static void print(int nums[]) {
+//     //     for(int num : nums) 
+//     //     {
+//     //         System.out.print(nums+" ");
+//     //     }
+//     //     System.out.println();
+//     // }
+}
 }
