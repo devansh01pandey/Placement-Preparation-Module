cc30d89987456f1e41c98ab0278517252f6c4311
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-07-07 23:07:28 +0530

    Time: 8 ms (17.42%), Space: 51.2 MB (44.61%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,12 +1,27 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int res = nums.length;
-        for(int i=0;i<nums.length;i++)
+        // int res = nums.length;
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     res = res ^ i;
+        //     res = res ^ nums[i];
+        // }
+        // return res;
+        
+        Arrays.sort(nums);
+        int left = 0,right = nums.length,mid =(left+right)/2;
+        while(left<right)
         {
-            res = res ^ i;
-            res = res ^ nums[i];
+            mid = (left+right)/2;
+            if(nums[mid]>mid)
+            {
+                right = mid;
+            }
+            else{
+                left = mid+1;
+            }
         }
-        return res;
+        return left;
         
     }
 }
