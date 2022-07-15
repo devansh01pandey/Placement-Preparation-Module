94af0f5e640e8859678bbba3a33a57f2ae30f49c
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-07-15 22:32:40 +0530

    Time: 5 ms (70.27%), Space: 44.4 MB (35.83%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,40 +1,35 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<Integer>();
-        HashMap<Integer,Integer> greatest = new HashMap<Integer,Integer>();
-//         We will traverse from back
+        Stack<Integer> stk = new Stack<>();
+        HashMap <Integer,Integer> greatest = new HashMap<>();
+        
         for(int i = nums2.length-1;i>=0;i--)
         {
-            if(st.isEmpty())
-                greatest.put(nums2[i] , -1);
-            else if(st.isEmpty() == false && st.peek() > nums2[i])
-            {              
-                greatest.put(nums2[i] , st.peek());
-            }
-            
-            else if(!st.empty() && st.peek() <= nums2[i])
+            if(stk.isEmpty() == true)
             {
-                while(!st.empty() && st.peek() <= nums2[i])
+                stk.push(nums2[i]);
+                greatest.put(nums2[i] , -1);
+            }
+            else
+            {
+                while(stk.isEmpty() == false && stk.peek()<=nums2[i])
+                    stk.pop();
+                if(stk.isEmpty() == true)
                 {
-                    st.pop();
-                }
-                if(st.empty())
                     greatest.put(nums2[i] , -1);
+                    stk.push(nums2[i]);
+                }
                 else
                 {
-                    greatest.put(nums2[i],st.peek());
+                    greatest.put(nums2[i] , stk.peek());
+                    stk.push(nums2[i]);
                 }
+                
             }
-            st.push(nums2[i]);     
         }
-        
         int res[] = new int[nums1.length];
-        for(int i = 0;i<nums1.length;i++)
-        {
+        for(int i=0;i<nums1.length;i++)
             res[i] = greatest.get(nums1[i]);
-        }
         return res;
-        
-        
     }
 }
