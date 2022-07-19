2c2bce84def04744e718b3422e06780b5f37d4cd
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-07-19 14:45:54 +0530

    Time: 7 ms (67.56%), Space: 48.5 MB (51.94%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,40 +1,40 @@
 class MinStack {
-    class Node{
-        int val;
-        int min;
-        Node next;
-        
-        public Node (int val, int min, Node next)
-        {
-            this.val = val;
-            this.min = min;
-            this.next = next;
-        }
-    }
-    private Node head;
 
     public MinStack() {
-        head = null;
+        
     }
+    private Node head;
     
-    public void push(int val) {
-        if(head == null)
-            head = new Node(val,val,null);
-        else
-            head = new Node(val, Math.min(val, head.min), head);
-    }
-    
-    public void pop() {
-        head = head.next;        
-    }
-    
-    public int top() {
-        return head.val;        
-    }
-    
-    public int getMin() {
-        return head.min;        
-    }
+   public void push(int x) {
+   if(head == null) 
+      head = new Node(x, x, null);
+   else 
+      head = new Node(x, Math.min(x, head.min), head);
+}
+
+public void pop() {
+   head = head.next;
+}
+
+public int top() {
+   return head.val;
+}
+
+public int getMin() {
+   return head.min;
+}
+
+private class Node {
+   int val;
+   int min;
+   Node next;
+
+   private Node(int val, int min, Node next) {
+     this.val = val;
+     this.min = min;
+     this.next = next;
+}
+}
 }
 
 /**
