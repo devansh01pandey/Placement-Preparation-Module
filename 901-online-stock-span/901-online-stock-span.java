a60007984a0ffb0a04a58cfb9b72f088239edca7
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-07-19 12:48:02 +0530

    Time: 71 ms (40.22%), Space: 69.3 MB (67.99%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.java
+++ 901-online-stock-span/901-online-stock-span.java
@@ -1,17 +1,21 @@
 class StockSpanner {
     
-     Stack<int[]> stack = new Stack<>();
+    Stack<int[]> s;
 
-    public StockSpanner() {      
+    public StockSpanner() {
+        s = new Stack<>();      
         
     }
     
     public int next(int price) {
-        int res = 1;
-        while (!stack.isEmpty() && stack.peek()[0] <= price)
-            res += stack.pop()[1];
-        stack.push(new int[]{price, res});
-        return res;
+        
+        int span =1;
+        while(!s.isEmpty() && price >= s.peek()[0]){
+            span += s.pop()[1];
+        }
+        s.push(new int[]{price,span});
+        return span;
+        
     }
 }
 
