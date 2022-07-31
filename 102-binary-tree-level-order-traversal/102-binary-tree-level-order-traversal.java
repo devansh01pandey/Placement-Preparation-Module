23066cc727634e9a7fdd2949f3e87891ce3b04bc
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-07-31 21:44:17 +0530

    Time: 1 ms (93.13%), Space: 43.7 MB (47.57%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -15,21 +15,22 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<TreeNode> q = new LinkedList<>();
         List<List<Integer>> ans = new LinkedList<List<Integer>>();
         if(root == null)
             return ans;
+        Queue<TreeNode> q = new LinkedList<>();        
+        
         q.offer(root);
         while(!q.isEmpty()){
             int level = q.size();
-            // TreeNode cur = q.poll();
             List<Integer> subans = new ArrayList<>();
             for(int i =0;i<level;i++){
-                if(q.peek().left != null)
-                    q.offer(q.peek().left);
-                if(q.peek().right != null)
-                    q.offer(q.peek().right);
-                subans.add(q.poll().val);
+                TreeNode cur = q.poll();
+                if(cur.left != null)
+                    q.offer(cur.left);
+                if(cur.right != null)
+                    q.offer(cur.right);
+                subans.add(cur.val);
             }
             ans.add(subans);
         }
