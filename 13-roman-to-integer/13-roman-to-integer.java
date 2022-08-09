8ea99ecbd7e5ef660d8ca648c9a04f51b95da659
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-08-09 19:13:40 +0530

    Time: 6 ms (85.69%), Space: 44.7 MB (82.06%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.java
+++ 13-roman-to-integer/13-roman-to-integer.java
@@ -1,5 +1,6 @@
 class Solution {
     public int romanToInt(String s) {
+        if(s == null || s.length()==0) return 0;
         int ans =0, num = 0;
         for(int i=s.length()-1;i>=0;i--)
         {
