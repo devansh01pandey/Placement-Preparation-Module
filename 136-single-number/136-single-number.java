188ff2096fb428fac015432272a2b73458f414cd
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-08-27 02:17:05 +0530

    Time: 1 ms (100.00%), Space: 51.2 MB (30.93%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,8 +1,8 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int res=0;
-        for(int i =0;i<nums.length;i++){
-            res ^= nums[i];
+        for(int i: nums){
+            res ^= i;
         }
         return res;
         
