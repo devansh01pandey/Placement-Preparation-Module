0f8a632bc71f8be3668022da8f69b42075ed33e8
devansh01pandey <108690198+devansh01pandey@users.noreply.github.com>
2022-09-29 17:42:04 +0530

    Time: 1 ms (100.00%), Space: 81.2 MB (16.10%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -3,8 +3,8 @@ class Solution {
         
         int res = nums[0];
         int maxEnding = nums[0];
-        // int i =1;
-        for(int i=1;i<nums.length;i++)
+        int i =1;
+        for(i=1;i<nums.length;i++)
         {
             maxEnding = Math.max(maxEnding + nums[i],nums[i]);
             res = Math.max(res,maxEnding);
